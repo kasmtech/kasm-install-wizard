7924f882db288c01af5aefb7075a1d7b3ab23445
Chris Hunt <chris.hunt@kasmweb.com>
2026-03-31 14:01:08 +0100

    KASM-8264 Don't call setGpu when there aren't any images

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ io.on('connection', async function (socket) {
     }
 
     // GPU yaml merge
-    if (installSettings.forceGpu !== 'disabled') {
+    if (installSettings.forceGpu !== 'disabled' && imagesI.images) {
       imagesI = await setGpu(imagesI);
     }
 
@@ -236,7 +236,7 @@ io.on('connection', async function (socket) {
     }
 
     // GPU yaml merge
-    if (upgradeSettings.forceGpu !== 'disabled') {
+    if (upgradeSettings.forceGpu !== 'disabled' && imagesI.images) {
       imagesI = await setGpu(imagesI);
     }
 
